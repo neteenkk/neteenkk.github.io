e32093d24f8675b50823b120fc10c47accfb1caa
neteenkk <neteenkk@gmail.com>
2026-05-25 09:18:01 +0530

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,36 @@
+import type { Metadata, Site, Socials } from "@types";
+
+export const SITE: Site = {
+  TITLE: "Nk",
+  DESCRIPTION: "My Personal Blog.",
+  EMAIL: "nitinkumar21038@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION:
+    "A collection of my projects with links to repositories and live demos.",
+};
+
+export const SOCIALS: Socials = [
+  {
+    NAME: "GitHub",
+    HREF: "https://github.com/neteenkk",
+  },
+  {
+    NAME: "Website",
+    HREF: "https://neteenkk.github.io",
+  },
+];
---
+++ src/content/blog/01-sample-post/index.md
@@ -0,0 +1,9 @@
+---
+title: "Sample Post"
+description: ""
+date: "2026-05-25"
+tags:
+  - example
+---
+
+This post is sample post
